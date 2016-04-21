37362fad27c921695fc746b9f1766ee280bc9002
Joong-Hee Lee <leejoonghee@gmail.com>
2016-04-21 11:17:22 +0900

    Fixes #11

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
     
 setup(
     name='configbuilder',
-    version='0.2.1',
+    version='0.2.2',
     url='https://github.com/joongh/configbuilder',
     license='MIT',
     author='Joong-Hee Lee',
